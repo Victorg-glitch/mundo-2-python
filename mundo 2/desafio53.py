bcd5ae520ee0d9357fe3238244de4c13c9341db5
Victor Gabriel <Victorgabrilvc@gmail.com>
2026-07-23 06:53:52 -0300

    Nova atividade

--- mundo 2/desafio53.py
+++ mundo 2/desafio53.py
@@ -1,10 +1,16 @@
 
 frase = input('Digite uma frase:  ').strip().lower()
-palavra = frase.split()
-palindromo = ''.join(palavra)
-ultletra = len(palindromo) - 3
-print(len(palindromo))
-if palindromo[0:2] == palindromo[ultletra:]:
+#tratamento dos dados
+palavra = frase.split() #transformando em tabela
+frase_noespaco = ''.join(palavra) #tirando os espaços
+palindromo = frase_noespaco[:3] #pegando as primeiras 3 letras da frase
+frase_invertida = frase_noespaco[::-1] #invertendo as frases
+palindromo_invertido = frase_invertida[:3] #pegando as ultimas 3 letras invertidas.
+
+print(palindromo)
+
+#condição para saber se é um palindromo
+if palindromo == palindromo_invertido:
     print('Essa palavra é um palindromo')
 else:
     print('Essa palavra não é um palindromo')
---
+++ mundo 2/desafio56.py
@@ -0,0 +1,41 @@
+nome = []
+idade = []
+sexo = []
+menor21 = []
+
+for c in range(4): #passando informações de cada pessoa
+    nome.append(input('Digite o seu nome: '))
+    idade.append(int(input('Digite sua idade: ')))
+    sexo.append(input('M ou H: ').upper())
+
+#Criando o perfil de cada usuario
+pessoa1 = [nome[0], idade[0], sexo[0]]
+pessoa2 = [nome[1], idade[1], sexo[1]]
+pessoa3 = [nome[2], idade[2], sexo[2]]
+pessoa4 = [nome[3], idade[3], sexo[3]]
+
+#Vendo qual mulher tem menos de 21
+if idade[0] < 21 and sexo[0] == 'M':
+    menor21.append(idade[0])
+if idade[1] < 21 and sexo[1] == 'M':
+    menor21.append(idade[1])
+if idade[2] < 21 and sexo[2] == 'M':
+    menor21.append(idade[2])
+if idade[3] < 21 and sexo[3] == 'M':
+    menor21.append(idade[3])
+
+# Pegando a media das idades
+media = (idade[0] + idade[1] + idade[2] + idade[3]) / 4 
+
+#Descobrindo quem tem a maior idade
+if idade[0] > idade[1] and idade[0] > idade[2] and idade[0] > idade[3]: #pessoa1
+    maior_idade = nome[0]
+elif idade[1] > idade[0] and idade[1] > idade[2] and idade[1] > idade[3]: #pessoa2
+    maior_idade = nome[1]
+elif idade[2] > idade[0] and idade[2] > idade[1] and idade[2] > idade[3]: #pessoa3
+    maior_idade = nome[2]
+elif idade[3] > idade[0] and idade[3] > idade[1] and idade[3] > idade[2]: #pessoa4
+    maior_idade = nome[3]
+
+print(f'''a pessoa que tem a maior idade é {maior_idade}
+{len(menor21)} tem menos de 21 anos.''')
